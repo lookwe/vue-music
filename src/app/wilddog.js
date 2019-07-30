dc7ca090b99e01425b6b94ddfad5c0cd2675ad23
黄钟飞 <huangzhongfei@gymoo.cn>
2019-07-30 23:55:24 +0800

    修复版本更新问题

--- src/app/wilddog.js
+++ src/app/wilddog.js
@@ -4,7 +4,7 @@
 import * as wilddog from 'wilddog'
 
 const config = {
-  syncURL: 'https://wd1841756677tscqzo.wilddogio.com/'
+  syncURL: 'https://wd9778448440bwshfi.wilddogio.com/'
 }
 wilddog.initializeApp(config)
 export default wilddog.sync().ref('web')
